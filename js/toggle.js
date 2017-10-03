bc1ab68274ee8fc5767167e4c1dabae7b17c740b
Anatoly Dolgov <anatoldd@list.ru>
2017-10-03 23:11:09 +0600

    Index pixel-perfect

--- js/toggle.js
+++ js/toggle.js
@@ -3,7 +3,8 @@
 
   // navMain.classList.remove('main-nav--nojs');
 
-  menuToggle.addEventListener('click', function() {
+menuToggle.addEventListener('click', function() {
+
     if (mainMenu.classList.contains('main-menu--closed')) {
       mainMenu.classList.remove('main-menu--closed');
       mainMenu.classList.add('main-menu--opened');
@@ -11,4 +12,17 @@
       mainMenu.classList.add('main-menu--closed');
       mainMenu.classList.remove('main-menu--opened');
     }
-  });
+});
+
+menuToggle.addEventListener ("click", function (evt) {
+  menuToggle.classList.toggle("page-header__toggle--burger");
+
+  if (menuToggle.classList.contains("page-header__toggle--cross")) {
+      menuToggle.classList.remove("page-header__toggle--cross");
+      menuToggle.classList.add('page-header__toggle--burger');
+  } else {
+    menuToggle.classList.add('page-header__toggle--cross');
+    menuToggle.classList.remove('page-header__toggle--burger');
+  }
+
+});
