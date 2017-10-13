7bad791cae6235544fcff8657742635b9b9585c2
Anatoly Dolgov <anatoldd@list.ru>
2017-10-13 17:08:27 +0600

    chngs

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,6 @@ gulp.task("serve", ["style"], function() {
     ui: false
   });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("sass/**/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
