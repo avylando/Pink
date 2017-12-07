2a09e197858a7c1d1ff92c533af3ee4dbc6edeee
Anatoly Dolgov <anatoldd@list.ru>
2017-12-07 15:45:09 +0500

    js

--- docs/js/form.js
+++ docs/js/form.js
@@ -1,8 +1,10 @@
 var buttonForm = document.querySelector(".fields__button");
 var popupSend = document.querySelector(".modal--send");
+var popupInvalid = document.querySelector(".modal--invalid");
 var popupError = document.querySelector(".modal--error");
 var buttonClose = document.querySelector(".modal__button--close");
 var buttonOk = document.querySelector(".modal__button--ok");
+var buttonAccept = document.querySelector(".modal__button--accept");
 var form = document.querySelector(".fields");
 var nameField = form.querySelector("[name=name]");
 var surnameField = form.querySelector("[name=surname]");
@@ -15,22 +17,22 @@ form.addEventListener("submit", function (evt) {
     console.log("Не заполнены необходимые поля");
     if (!nameField.value) {
       nameField.classList.add("fields__textfield--required");
-      popupError.classList.add("modal__show--error");
+      popupInvalid.classList.add("modal__show--invalid");
       blackout.classList.add("blackout--show");
-    } else {nameField.classList.remove("fields__textfield--required")};
+    } else { nameField.classList.remove("fields__textfield--required") };
     if (!surnameField.value) {
       surnameField.classList.add("fields__textfield--required");
-      popupError.classList.add("modal__show--error");
+      popupInvalid.classList.add("modal__show--invalid");
       blackout.classList.add("blackout--show");
-    } else {surnameField.classList.remove("fields__textfield--required")};
+    } else { surnameField.classList.remove("fields__textfield--required") };
     if (!emailField.value) {
       emailField.classList.add("fields__textfield--required");
-      popupError.classList.add("modal__show--error");
+      popupInvalid.classList.add("modal__show--invalid");
       blackout.classList.add("blackout--show");
-    } else {emailField.classList.remove("fields__textfield--required")};
+    } else { emailField.classList.remove("fields__textfield--required") };
   } else {
     evt.preventDefault();
-    popupError.classList.remove("modal__show--error");
+    popupInvalid.classList.remove("modal__show--invalid");
     popupSend.classList.add("modal__show--send");
     blackout.classList.add("blackout--show");
     nameField.classList.remove("fields__textfield--required");
@@ -42,19 +44,18 @@ form.addEventListener("submit", function (evt) {
   }
 });
 
-buttonClose.addEventListener("click", function (evt) {
-  if (popupSend.classList.contains("modal__show--send")) {
-    popupSend.classList.remove("modal__show--send");
-    blackout.classList.remove("blackout--show");
-  }
-});
+var modalClickHandler = function (button, modal, className) {
+  button.addEventListener('click', function () {
+    if (modal.classList.contains(className)) {
+      modal.classList.remove(className);
+      blackout.classList.remove('blackout--show');
+    }
+  })
+};
 
-buttonOk.addEventListener("click", function (evt) {
-  if (popupError.classList.contains("modal__show--error")) {
-    popupError.classList.remove("modal__show--error");
-    blackout.classList.remove("blackout--show");
-  }
-})
+modalClickHandler(buttonClose, popupSend, 'modal__show--send');
+modalClickHandler(buttonOk, popupInvalid, 'modal__show--invalid');
+modalClickHandler(buttonAccept, popupInvalid, 'modal__show--error');
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
@@ -62,6 +63,7 @@ window.addEventListener("keydown", function (evt) {
       popupSend.classList.remove("modal__show--send");
       blackout.classList.remove("blackout--show");
     }
+
     if (popupError.classList.contains("modal__show--error")) {
       popupError.classList.remove("modal__show--error");
       blackout.classList.remove("blackout--show");
